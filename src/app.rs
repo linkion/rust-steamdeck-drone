ae5f5b1389ddf8030f443684750f6d68a70e284e
linkion <42103411+linkion@users.noreply.github.com>
2023-03-28 17:34:03 -0500

    fix

--- src/app.rs
+++ src/app.rs
@@ -77,19 +77,17 @@ impl eframe::App for TemplateApp {
             }
 
             let mut active_gamepad = None;
+            
+            // Examine new events
+            while let Some(Event { id, event, time }) = gilrs.next_event() {
+                ui.label(format!("{:?} New event from {}: {:?}", time, id, event));
+                active_gamepad = Some(id);
+            }
 
-            loop {
-                // Examine new events
-                while let Some(Event { id, event, time }) = gilrs.next_event() {
-                    ui.label(format!("{:?} New event from {}: {:?}", time, id, event));
-                    active_gamepad = Some(id);
-                }
-
-                // You can also use cached gamepad state
-                if let Some(gamepad) = active_gamepad.map(|id| gilrs.gamepad(id)) {
-                    if gamepad.is_pressed(Button::South) {
-                        ui.label("Button South is pressed (XBox - A, PS - X)");
-                    }
+            // You can also use cached gamepad state
+            if let Some(gamepad) = active_gamepad.map(|id| gilrs.gamepad(id)) {
+                if gamepad.is_pressed(Button::South) {
+                    ui.label("Button South is pressed (XBox - A, PS - X)");
                 }
             }
 
